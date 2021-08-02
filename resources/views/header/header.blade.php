8b9c09bc3d3f4ed06c30403402fb60dc0cb2e581
Anggit Prayogo <anggitprayogo@gmail.com>
2021-08-02 17:54:45 +0700

    tweak header

--- resources/views/header/header.blade.php
+++ resources/views/header/header.blade.php
@@ -1,10 +1,10 @@
 <div class="header" id="home">
 	<div class="container">
 		<ul>
-		    <li> <a href="#" data-toggle="modal" data-target="#myModal"><i class="fa fa-unlock-alt" aria-hidden="true"></i> Sign In </a></li>
-			<li> <a href="#" data-toggle="modal" data-target="#myModal2"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Sign Up </a></li>
-			<li><i class="fa fa-phone" aria-hidden="true"></i> Call : 01234567898</li>
-			<li><i class="fa fa-envelope-o" aria-hidden="true"></i> <a href="mailto:info@example.com">info@example.com</a></li>
+		    <li> <a href="#" data-toggle="modal" data-target="#myModal"><i class="fa fa-unlock-alt" aria-hidden="true"></i> Cek Order </a></li>
+			<li><i class="fa fa-phone" aria-hidden="true"></i> Hubungi : 01234567898</li>
+			<li><i class="fa fa-whatsapp" aria-hidden="true"></i> Wa : 01234567898</li>
+			<li><i class="fa fa-envelope-o" aria-hidden="true"></i> <a href="mailto:info@example.com">kilalayugrow@gmail.com</a></li>
 		</ul>
 	</div>
 </div>
