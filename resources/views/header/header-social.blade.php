28aa7c6b8e0ac8d66777220d33b48419c9726119
Anggit Prayogo <anggitprayogo@gmail.com>
2021-08-09 06:35:05 +0700

    tweak headear

--- resources/views/header/header-social.blade.php
+++ resources/views/header/header-social.blade.php
@@ -2,25 +2,26 @@
 <div class="header-bot">
 	<div class="header-bot_inner_wthreeinfo_header_mid">
 		<!-- header-bot -->
-                <div class="col-md-3 logo_agile">
-                        <h1><a href="index.html">KilalayuGrow</i></a></h1>
+                <div class="col-md-4 logo_agile">
+                        <img src="{{URL::asset('/images/logo_kilalayu.png')}}" alt="Kilalayu Grow">
+                        <h1><a href="">KilalayuGrow</i></a></h1>
                 </div>
                 <!-- header-bot -->
-                <div class="col-md-9 agileits-social top_content">
+                <div class="col-md-8 agileits-social top_content">
                         <ul class="social-nav model-3d-0 footer-social w3_agile_social">
-                        <li class="share">Bagikan : </li>
-                        <li><a href="#" class="facebook">
-                                <div class="front"><i class="fa fa-facebook" aria-hidden="true"></i></div>
-                                <div class="back"><i class="fa fa-facebook" aria-hidden="true"></i></div></a></li>
-                        <li><a href="#" class="twitter"> 
-                                <div class="front"><i class="fa fa-twitter" aria-hidden="true"></i></div>
-                                <div class="back"><i class="fa fa-twitter" aria-hidden="true"></i></div></a></li>
-                        <li><a href="#" class="instagram">
+                        <li class="share">Contact Us : </li>
+                        <li><a href="https://api.whatsapp.com/send?phone=+6285814443037&text=Halo%20Kak,%20aku%20mau%20tanya%20sablon%20nih" class="whatsapp">
+                                <div class="front"><i class="fa fa-whatsapp" aria-hidden="true"></i></div>
+                                <div class="back"><i class="fa fa-whatsapp" aria-hidden="true"></i></div></a></li>
+                        <li><a href="https://www.instagram.com/kilalayugrow/" class="instagram"> 
                                 <div class="front"><i class="fa fa-instagram" aria-hidden="true"></i></div>
                                 <div class="back"><i class="fa fa-instagram" aria-hidden="true"></i></div></a></li>
-                        <li><a href="#" class="pinterest">
-                                <div class="front"><i class="fa fa-linkedin" aria-hidden="true"></i></div>
-                                <div class="back"><i class="fa fa-linkedin" aria-hidden="true"></i></div></a></li>
+                        <li><a href="https://web.facebook.com/kilalayugrow" class="facebook">
+                                <div class="front"><i class="fa fa-facebook" aria-hidden="true"></i></div>
+                                <div class="back"><i class="fa fa-facebook" aria-hidden="true"></i></div></a></li>
+                        <li><a href="mailto:kilalayugrow@gmail.com?subject=Contact%20Us&body=Halo%20Kak,%20aku%20mau%20tanya%20sablon%20nih" class="pinterest">
+                                <div class="front"><i class="fa fa-envelope" aria-hidden="true"></i></div>
+                                <div class="back"><i class="fa fa-envelope" aria-hidden="true"></i></div></a></li>
                         </ul>
 		</div>
 		<div class="clearfix"></div>
--- resources/views/header/header.blade.php
+++ resources/views/header/header.blade.php
@@ -1,10 +1,8 @@
 <div class="header" id="home">
-	<div class="container">
+	<div class="container text-center">
 		<ul>
-		    <li> <a href="#" data-toggle="modal" data-target="#myModal"><i class="fa fa-unlock-alt" aria-hidden="true"></i> Cek Order </a></li>
-			<li><i class="fa fa-phone" aria-hidden="true"></i> Hubungi : 01234567898</li>
-			<li><i class="fa fa-whatsapp" aria-hidden="true"></i> Wa : 01234567898</li>
-			<li><i class="fa fa-envelope-o" aria-hidden="true"></i> <a href="mailto:info@example.com">kilalayugrow@gmail.com</a></li>
+			<li><i class="fa fa-whatsapp" aria-hidden="true"></i>01234567898</li>
+			<li><i class="fa fa-instagram" aria-hidden="true"></i>@kilalayugrow</li>
 		</ul>
 	</div>
 </div>
