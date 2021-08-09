0cd002dca2ba205a82aaef9d88ba55c732a05a0c
Anggit Prayogo <anggitprayogo@gmail.com>
2021-08-09 07:09:24 +0700

    Add faq template

--- resources/views/content/home.blade.php
+++ resources/views/content/home.blade.php
@@ -75,156 +75,42 @@ Home
 
 <!-- /banner_bottom_agile_info -->
 <div class="banner_bottom_agile_info">
-    <div class="container">
-        <div class="agile_ab_w3ls_info">
-            <div class="col-md-6 ab_pic_w3ls">
-                <img src="{{url('/images/ab_pic.jpg')}}" alt=" " class="img-responsive" />
-            </div>
-                <div class="col-md-6 ab_pic_w3ls_text_info">
-                <h5>About Our Elite <span> Shoppy</span> </h5>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elPellentesque vehicula augue eget nisl ullamcorper, molestie blandit ipsum auctor. Mauris volutpat augue dolor..</p>
-                <p>Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut lab ore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. labore et dolore magna aliqua.</p>
-            </div>
-                <div class="clearfix"></div>
-        </div>    
+    <div class="container">    
         <div class="banner_bottom_agile_info_inner_w3ls">
-                <div class="col-md-6 wthree_banner_bottom_grid_three_left1 grid">
-                    <figure class="effect-roxy">
-                        <img src="images/bottom1.jpg" alt=" " class="img-responsive" />
-                        <figcaption>
-                            <h3><span>F</span>all Ahead</h3>
-                            <p>New Arrivals</p>
-                        </figcaption>			
-                    </figure>
-                </div>
-                    <div class="col-md-6 wthree_banner_bottom_grid_three_left1 grid">
-                    <figure class="effect-roxy">
-                        <img src="images/bottom2.jpg" alt=" " class="img-responsive" />
-                        <figcaption>
-                            <h3><span>F</span>all Ahead</h3>
-                            <p>New Arrivals</p>
-                        </figcaption>			
-                    </figure>
-                </div>
-                <div class="clearfix"></div>
+			<div class="col-md-6 wthree_banner_bottom_grid_three_left1 grid">
+				<figure class="effect-roxy">
+					<img src="images/bottom1.jpg" alt=" " class="img-responsive" />
+					<figcaption>
+						<h3><span>F</span>lyer</h3>
+						<p>Promo 1</p>
+					</figcaption>			
+				</figure>
+			</div>
+			<div class="col-md-6 wthree_banner_bottom_grid_three_left1 grid">
+				<figure class="effect-roxy">
+					<img src="images/bottom2.jpg" alt=" " class="img-responsive" />
+					<figcaption>
+						<h3><span>F</span>lyer</h3>
+						<p>Promo 2</p>
+					</figcaption>			
+				</figure>
+			</div>
+			<div class="clearfix"></div>
         </div> 
+		
+		
+		<div class="col-md-12 wthree_banner_bottom_grid_three_left1 grid" style="margin-top:20px">
+			<figure class="effect-roxy">
+				<img src="images/bottom2.jpg" alt=" " class="img-responsive" />
+				<figcaption>
+					<h3><span>F</span>lyer</h3>
+					<p>Promo 3</p>
+				</figcaption>			
+			</figure>
+		</div>
+		
 	</div> 
 </div>
-<!-- team -->
-
-
-<div class="banner_bottom_agile_info team">
-	<div class="container">
-	            <h3 class="wthree_text_info">Our Team <span>Members</span></h3>
-			<div class="inner_w3l_agile_grids">
-					<div class="col-md-3 team-grids">
-						<div class="thumbnail team-w3agile">
-							<img src="images/t1.jpg" class="img-responsive" alt="">
-							<div class="social-icons team-icons right-w3l fotw33">
-							<div class="caption">
-								<h4>Joanna Vilken</h4>
-								<p>Add Short Description</p>						
-							</div>
-								<ul class="social-nav model-3d-0 footer-social w3_agile_social two">
-                                    <li><a href="#" class="facebook">
-                                            <div class="front"><i class="fa fa-facebook" aria-hidden="true"></i></div>
-                                            <div class="back"><i class="fa fa-facebook" aria-hidden="true"></i></div></a></li>
-                                    <li><a href="#" class="twitter"> 
-                                            <div class="front"><i class="fa fa-twitter" aria-hidden="true"></i></div>
-                                            <div class="back"><i class="fa fa-twitter" aria-hidden="true"></i></div></a></li>
-                                    <li><a href="#" class="instagram">
-                                            <div class="front"><i class="fa fa-instagram" aria-hidden="true"></i></div>
-                                            <div class="back"><i class="fa fa-instagram" aria-hidden="true"></i></div></a></li>
-                                    <li><a href="#" class="pinterest">
-                                            <div class="front"><i class="fa fa-linkedin" aria-hidden="true"></i></div>
-                                            <div class="back"><i class="fa fa-linkedin" aria-hidden="true"></i></div></a></li>
-                                </ul>
-							          </div>
-						     </div>
-
-			         	</div>
-						<div class="col-md-3 team-grids">
-						<div class="thumbnail team-w3agile">
-							<img src="images/t2.jpg" class="img-responsive" alt="">
-							<div class="social-icons team-icons right-w3l fotw33">
-							<div class="caption">
-								<h4>Anika Mollik</h4>
-								<p>Add Short Description</p>						
-							</div>
-								<ul class="social-nav model-3d-0 footer-social w3_agile_social two">
-															<li><a href="#" class="facebook">
-																  <div class="front"><i class="fa fa-facebook" aria-hidden="true"></i></div>
-																  <div class="back"><i class="fa fa-facebook" aria-hidden="true"></i></div></a></li>
-															<li><a href="#" class="twitter"> 
-																  <div class="front"><i class="fa fa-twitter" aria-hidden="true"></i></div>
-																  <div class="back"><i class="fa fa-twitter" aria-hidden="true"></i></div></a></li>
-															<li><a href="#" class="instagram">
-																  <div class="front"><i class="fa fa-instagram" aria-hidden="true"></i></div>
-																  <div class="back"><i class="fa fa-instagram" aria-hidden="true"></i></div></a></li>
-															<li><a href="#" class="pinterest">
-																  <div class="front"><i class="fa fa-linkedin" aria-hidden="true"></i></div>
-																  <div class="back"><i class="fa fa-linkedin" aria-hidden="true"></i></div></a></li>
-														</ul>
-							          </div>
-						     </div>
-			         	</div>
-						<div class="col-md-3 team-grids">
-						<div class="thumbnail team-w3agile">
-							<img src="images/t3.jpg" class="img-responsive" alt="">
-							<div class="social-icons team-icons right-w3l fotw33">
-							<div class="caption">
-								<h4>Megali Deo</h4>
-								<p>Add Short Description</p>						
-							</div>
-								<ul class="social-nav model-3d-0 footer-social w3_agile_social two">
-															<li><a href="#" class="facebook">
-																  <div class="front"><i class="fa fa-facebook" aria-hidden="true"></i></div>
-																  <div class="back"><i class="fa fa-facebook" aria-hidden="true"></i></div></a></li>
-															<li><a href="#" class="twitter"> 
-																  <div class="front"><i class="fa fa-twitter" aria-hidden="true"></i></div>
-																  <div class="back"><i class="fa fa-twitter" aria-hidden="true"></i></div></a></li>
-															<li><a href="#" class="instagram">
-																  <div class="front"><i class="fa fa-instagram" aria-hidden="true"></i></div>
-																  <div class="back"><i class="fa fa-instagram" aria-hidden="true"></i></div></a></li>
-															<li><a href="#" class="pinterest">
-																  <div class="front"><i class="fa fa-linkedin" aria-hidden="true"></i></div>
-																  <div class="back"><i class="fa fa-linkedin" aria-hidden="true"></i></div></a></li>
-														</ul>
-							          </div>
-						     </div>
-			         	</div>
-						<div class="col-md-3 team-grids">
-						<div class="thumbnail team-w3agile">
-							<img src="images/t4.jpg" class="img-responsive" alt="">
-							<div class="social-icons team-icons right-w3l fotw33">
-							<div class="caption">
-								<h4>Retas Word</h4>
-								<p>Add Short Description</p>						
-							</div>
-								<ul class="social-nav model-3d-0 footer-social w3_agile_social two">
-															<li><a href="#" class="facebook">
-																  <div class="front"><i class="fa fa-facebook" aria-hidden="true"></i></div>
-																  <div class="back"><i class="fa fa-facebook" aria-hidden="true"></i></div></a></li>
-															<li><a href="#" class="twitter"> 
-																  <div class="front"><i class="fa fa-twitter" aria-hidden="true"></i></div>
-																  <div class="back"><i class="fa fa-twitter" aria-hidden="true"></i></div></a></li>
-															<li><a href="#" class="instagram">
-																  <div class="front"><i class="fa fa-instagram" aria-hidden="true"></i></div>
-																  <div class="back"><i class="fa fa-instagram" aria-hidden="true"></i></div></a></li>
-															<li><a href="#" class="pinterest">
-																  <div class="front"><i class="fa fa-linkedin" aria-hidden="true"></i></div>
-																  <div class="back"><i class="fa fa-linkedin" aria-hidden="true"></i></div></a></li>
-														</ul>
-							          </div>
-						     </div>
-			         	</div>
-					<div class="clearfix"> </div>
-				</div>
-	       </div>
-		</div>
-<!-- //team -->
-
-
 
 <!-- schedule-bottom -->
 <div class="schedule-bottom">
@@ -233,23 +119,12 @@ Home
     </div>
     <div class="col-md-6 agileits_schedule_bottom_right">
         <div class="w3ls_schedule_bottom_right_grid">
-            <h3>Save up to <span>50%</span> in this week</h3>
-            <p>Suspendisse varius turpis efficitur erat laoreet dapibus. 
-                Mauris sollicitudin scelerisque commodo.Nunc dapibus mauris sed metus finibus posuere.</p>
+            <h3>Statistik Kami</h3>
+            <p>Kami mencoba memberikan yang terbaik</p>
             <div class="col-md-4 w3l_schedule_bottom_right_grid1">
                 <i class="fa fa-user-o" aria-hidden="true"></i>
-                <h4>Customers</h4>
+                <h4>Pelanggan</h4>
                 <h5 class="counter">653</h5>
-            </div>
-            <div class="col-md-4 w3l_schedule_bottom_right_grid1">
-                <i class="fa fa-calendar-o" aria-hidden="true"></i>
-                <h4>Events</h4>
-                <h5 class="counter">823</h5>
-            </div>
-            <div class="col-md-4 w3l_schedule_bottom_right_grid1">
-                <i class="fa fa-shield" aria-hidden="true"></i>
-                <h4>Awards</h4>
-                <h5 class="counter">45</h5>
             </div>
             <div class="clearfix"> </div>
         </div>
@@ -257,10 +132,12 @@ Home
     <div class="clearfix"> </div>
 </div>
 <!-- //schedule-bottom -->
+
+
 <!-- banner-bootom-w3-agileits -->
 <div class="banner-bootom-w3-agileits">
     <div class="container">
-        <h3 class="wthree_text_info">What's <span>Trending</span></h3>
+        <h3 class="wthree_text_info">Portofolio <span>Kami</span></h3>
 
     <div class="col-md-5 bb-grids bb-left-agileits-w3layouts">
         <a href="women.html">
@@ -299,29 +176,6 @@ Home
 </div>
 </div>
 
-
-<!--/grids-->
-<div class="agile_last_double_sectionw3ls">
-            <div class="col-md-6 multi-gd-img multi-gd-text ">
-					<a href="womens.html"><img src="images/bot_1.jpg" alt=" "><h4>Flat <span>50%</span> offer</h4></a>
-					
-			</div>
-			 <div class="col-md-6 multi-gd-img multi-gd-text ">
-					<a href="womens.html"><img src="images/bot_2.jpg" alt=" "><h4>Flat <span>50%</span> offer</h4></a>
-			</div>
-			<div class="clearfix"></div>
-	   </div>							
-<!--/grids-->
-	<!-- /we-offer -->
-		<div class="sale-w3ls">
-			<div class="container">
-				<h6>We Offer Flat <span>40%</span> Discount</h6>
- 
-				<a class="hvr-outline-out button2" href="single.html">Shop Now </a>
-			</div>
-		</div>
-	<!-- //we-offer -->
-<!--/grids-->
 <div class="coupons">
 		<div class="coupons-grids text-center">
 			<div class="w3layouts_mail_grid">
@@ -367,4 +221,56 @@ Home
 		</div>
 </div>
 <!--grids-->
+
+
+
+
+<!-- FAQ -->
+<div class="banner-bootom-w3-agileits">
+	<div class="container">
+		<h3 class="wthree_text_info">FAQ </h3>
+		<div class="panel-group" id="accordion">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h4 class="panel-title">
+						<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">Is account registration required?</a>
+					</h4>
+				</div>
+				<div id="collapseOne" class="panel-collapse collapse in">
+					<div class="panel-body">
+						Account registration at <strong>PrepBootstrap</strong> is only required if you will be selling or buying themes. 
+						This ensures a valid communication channel for all parties involved in any transactions. 
+					</div>
+				</div>
+			</div>
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h4 class="panel-title">
+						<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTen">Can I submit my own Bootstrap templates or themes?</a>
+					</h4>
+				</div>
+				<div id="collapseTen" class="panel-collapse collapse">
+					<div class="panel-body">
+						A lot of the content of the site has been submitted by the community. Whether it is a commercial element/template/theme 
+						or a free one, you are encouraged to contribute. All credits are published along with the resources. 
+					</div>
+				</div>
+			</div>
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h4 class="panel-title">
+						<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseEleven">What is the currency used for all transactions?</a>
+					</h4>
+				</div>
+				<div id="collapseEleven" class="panel-collapse collapse">
+					<div class="panel-body">
+						All prices for themes, templates and other items, including each seller's or buyer's account balance are in <strong>USD</strong>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</div>
+<!-- END FAQ -->
 @endsection('content')
