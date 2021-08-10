5cf54e6a7cea51e9c0bca89d6ae94f4e53e4085f
Anggit Prayogo <anggitprayogo@gmail.com>
2021-08-10 08:15:56 +0700

    Change phone number in header

--- resources/views/header/header.blade.php
+++ resources/views/header/header.blade.php
@@ -1,7 +1,7 @@
 <div class="header" id="home">
 	<div class="container text-center">
 		<ul>
-			<li><i class="fa fa-whatsapp" aria-hidden="true"></i>01234567898</li>
+			<li><i class="fa fa-whatsapp" aria-hidden="true"></i>+6285814443037</li>
 			<li><i class="fa fa-instagram" aria-hidden="true"></i>@kilalayugrow</li>
 		</ul>
 	</div>
